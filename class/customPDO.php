1e61a0adfdc46bb3bf7865cb0f7848166d8afb5a
luohancai <luohancai@taqu.cn>
2018-11-14 11:47:16 +0800

    pdo连接添加编码

--- class/customPDO.php
+++ class/customPDO.php
@@ -7,7 +7,7 @@ class customPDO extends \PDO
         $dns = $config['driver'] .
             ':host=' . $config['host'] .
             ((!empty($config['port'])) ? (';port=' . $config['port']) : '') .
-            ';dbname=' . $config['database'];
+            ';dbname=' . $config['database'].';charset='.$config['charset'];
         parent::__construct($dns, $config['username'], $config['password'], [
             parent::ATTR_EMULATE_PREPARES => false,
         ]);
